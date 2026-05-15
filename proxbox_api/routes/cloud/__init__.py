cbb0bf7a33c287e51c846bdca1f27f5c9f9e4f14
Emerson Felipe <emerson@netdevopsbr.com>
2026-05-15 20:14:12 +0000

    feat(cloud): build Proxmox template images

--- proxbox_api/routes/cloud/__init__.py
+++ proxbox_api/routes/cloud/__init__.py
@@ -1,6 +1,7 @@
 """Cloud Portal provisioning routes."""
 
 from proxbox_api.routes.cloud.provision import router as provision_router
+from proxbox_api.routes.cloud.template_images import router as template_images_router
 from proxbox_api.routes.cloud.templates import router as templates_router
 
-__all__ = ("provision_router", "templates_router")
+__all__ = ("provision_router", "template_images_router", "templates_router")
